323d2aefda1c30fbe37bfcce345b3152548d8253
Nav_InverseInfinity <98486154+nav-InverseInfinity@users.noreply.github.com>
2022-03-27 18:38:35 +0100

    To display  ip and port of the running container

---
+++ kube-ip-port.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This script is to display the ip and port where the website is up and running!!!
+
+#Kubernetes deployment name - "web"
+
+check="web"
+
+nodeport=$(kubectl get svc | grep web | awk '{print $5}' | cut -d ':' -f 2 | sed 's#/TCP##g')
+
+for i in $(kubectl get svc | cut -d ' ' -f 1)
+do
+        if [ $i == $check ]; then
+                echo "Try "$(curl -s ifconfig.me)":"$nodeport" ip & port to see the website running on the Kubernetes Cluster"
+        fi
+
+done
